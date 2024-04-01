08bb49a99e65e40b66b19f25a16c37bb4f889713
vademichev <129170477+vademichev@users.noreply.github.com>
2024-04-01 14:17:46 +0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -14,17 +14,18 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1'; // Имя кэша - как хранилища
+const PRECACHE = 'precache-v2'; // Имя кэша - как хранилища
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 // Что мы хотим кэшить
 const PRECACHE_URLS = [
   'index.html',
-  './', // Alias for index.html
-  'styles.css',
+  'style.css',
+  'logo192.jpg',
+  'logo512.jpg'
 ];
-
+//  './', // Alias for index.html
 // The install handler takes care of precaching the resources we always need.
 // Обработчик событий при установке приложения - он должен записать все в кэш заранее
 self.addEventListener('install', event => {
