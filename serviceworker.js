ef850f6d17bdb97c703cf2286ff929bda2bacf68
vademichev <129170477+vademichev@users.noreply.github.com>
2024-04-01 14:30:39 +0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v4'; // Имя кэша - как хранилища
+const PRECACHE = 'precache-v5'; // Имя кэша - как хранилища
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
